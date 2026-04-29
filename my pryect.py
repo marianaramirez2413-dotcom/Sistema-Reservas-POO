511a4c1827f99a0e5fd4cc852b3bf50dae184192
KewinR07 <kerestrepoc@unadvirtual.edu.co>
2026-04-29 11:42:51 -0500

    Se agrega clase abstracta Servicio - estructura base

---
+++ my pryect.py
@@ -0,0 +1,57 @@
+# =========================
+# IMPORTS
+# =========================
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+# =========================
+# LOGS
+# =========================
+def registrar_log(mensaje):
+    with open("log.txt", "a") as archivo:
+        archivo.write(f"{datetime.now()} - {mensaje}\n")
+
+# =========================
+# EXCEPCIÓN PERSONALIZADA
+# =========================
+class ErrorValidacion(Exception):
+    pass
+
+# =========================
+# CLASE CLIENTE
+# =========================
+class Cliente:
+    def __init__(self, nombre, correo):
+        if not nombre:
+            raise ErrorValidacion("Nombre vacío")
+        if "@" not in correo:
+            raise ErrorValidacion("Correo inválido")
+
+        self.__nombre = nombre
+        self.__correo = correo
+
+    def mostrar_info(self):
+        return f"Cliente: {self.__nombre} - {self.__correo}"
+
+
+# PRUEBA
+if __name__ == "__main__":
+    try:
+        c1 = Cliente("Juan", "juan@mail.com")
+        print(c1.mostrar_info())
+
+        c2 = Cliente("", "malcorreo")  # error
+    except Exception as e:
+        print("Error:", e)
+        registrar_log(e)
+
+        # =========================
+# CLASE ABSTRACTA SERVICIO
+# =========================
+class Servicio(ABC):
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    @abstractmethod
+    def calcular_costo(self):
+        pass
